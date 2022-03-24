a7e119057adcce4e4c4cef13e16ec602ba252d82
jyns1703 <jyns1703@usp.br>
2022-03-24 17:36:17 -0300

    Make the defender route random with bigger weigh when movig vertically

--- src/defender.c
+++ src/defender.c
@@ -1,5 +1,7 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Internal headers
 #include "direction.h"
@@ -19,11 +21,34 @@
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
   // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+  UNUSED(attacker_spy);
+  
+  srand(time(NULL));
+  
+  int ran = abs(rand() * (defender_position.i + defender_position.j)) ;
+  int dir;
+  
+  if (defender_position.j == 8){
+    dir = ran%7;
+  } 
+  else{
+    dir = ran%10;
+  }
+  direction_t direction_with_weigh_defender[10] = {
+    DIR_UP, // weigh 2
+    DIR_DOWN, // weigh 2
+    DIR_DOWN_LEFT,  
+    DIR_LEFT,       
+    DIR_UP_LEFT,
+    DIR_UP_RIGHT,
+    DIR_RIGHT,
+    DIR_DOWN_RIGHT,
+
+    DIR_UP,
+    DIR_DOWN,};
+    
+  return direction_with_weigh_defender[dir]; 
 }
 
 /*----------------------------------------------------------------------------*/
